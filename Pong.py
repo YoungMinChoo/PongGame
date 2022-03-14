6f318736865fa0334da7de154934119ab7ddd0c5
YoungMinChoo <youngminchoo001@gmail.com>
2022-03-14 19:17:35 +0900

    removed while loop in favor of ontimer   moved ball movement code into function

--- Pong.py
+++ Pong.py
@@ -1,4 +1,5 @@
 import turtle as t
+import winsound
 
 
 # Movement
@@ -25,19 +26,19 @@ def rightplayerdown():
     y = y - 40
     rightPaddle.sety(y)
 
+
 # Declarations
-playerAScore = 0
-playerBScore = 0
+ball_speed = 1
+base_speed = 0.4
+playerAScore, playerBScore = 0, 0
 bounces = 0
-ballSpeed = 1
 
 # Make the screen
 window = t.Screen()
 window.title("Pong Game")
 window.bgcolor("black")
-window.setup(width=800, height=800)
-window.tracer(1, 0)
-t.delay(0)
+window.setup(width=800, height=600)
+window.tracer(0)
 
 # Left Paddle
 leftPaddle = t.Turtle()
@@ -61,12 +62,10 @@ rightPaddle.goto(350, 0)
 ball = t.Turtle()
 ball.shape("circle")
 ball.color("white")
-ball.speed(3)
-ball.pen(speed=1)
+ball.speed(0)
 ball.penup()
 ball.goto(0, 0)
-ballXDirection = 0.2
-ballYDirection = 0.2
+ballXDirection, ballYDirection = base_speed, base_speed
 
 board = t.Turtle()
 board.speed(0)
@@ -76,18 +75,20 @@ board.hideturtle()
 board.goto(0, 260)
 board.write(
     "                          Score \nPlayer A: {}      Bounces: {}      Player B: {} \n                          Speed = {} ".format(
-        0, 0, 0, 0), align="center")
+        0, 0, 0, 1), align="center")
 window.listen()
 window.onkeypress(leftplayerup, 'w')
 window.onkeypress(leftplayerdown, 's')
 window.onkeypress(rightplayerup, 'Up')
 window.onkeypress(rightplayerdown, 'Down')
 
-while True:
-    window.update()
 
+def action():
+    global playerAScore, playerBScore, bounces, ballXDirection, ballYDirection, ball_speed
+    window.update()
     ball.setx(ball.xcor() + ballXDirection)
     ball.sety(ball.ycor() + ballYDirection)
+
     # Border set up
     # Vertical
     if ball.ycor() > 290:
@@ -101,10 +102,11 @@ while True:
     # Horizontal
     if ball.xcor() > 390:
         ball.goto(0, 0)
-        ball.pen(speed=1)
+        ballXDirection, ballYDirection = base_speed, base_speed
         ballXDirection = ballXDirection * -1
         playerAScore = playerAScore + 1
         bounces = 0
+        ball_speed = 1
         # Score
         board.clear()
         board.write(
@@ -113,10 +115,11 @@ while True:
 
     if (ball.xcor()) < -390:
         ball.goto(0, 0)
-        ball.pen(speed=1)
+        ballXDirection, ballYDirection = base_speed, base_speed
         ballXDirection = ballXDirection * -1
         playerBScore = playerBScore + 1
         bounces = 0
+        ball_speed = 1
         # Score
         board.clear()
         board.write(
@@ -128,28 +131,39 @@ while True:
             rightPaddle.ycor() + 40 > ball.ycor() > rightPaddle.ycor() - 40):
         ball.setx(340)
         ballXDirection = ballXDirection * -1
+        bounces = bounces + 1
 
         # High bounce difficulty
-        if bounces % 1 == 0:
-            ball.pen(speed=(ballSpeed + bounces + 1))
-        bounces = bounces + 1
+        if bounces % 2 == 0:
+            ballXDirection *= 1.2
+            ballYDirection *= 1.2
+            ball_speed += 1
+
 
         board.clear()
         board.write(
             "                          Score \nPlayer A: {}      Bounces: {}      Player B: {} \n                          Speed = {} ".format(
-                playerAScore, bounces, playerBScore, bounces + 1), align="center")
+                playerAScore, bounces, playerBScore, ball_speed), align="center")
 
-    if (ball.xcor() < -340) and (ball.xcor() > -350) and (
+    elif (ball.xcor() < -340) and (ball.xcor() > -350) and (
             leftPaddle.ycor() + 40 > ball.ycor() > leftPaddle.ycor() - 40):
         ball.setx(-340)
         ballXDirection = ballXDirection * -1
+        bounces = bounces + 1
 
         # High bounce difficulty
-        if bounces % 1 == 0:
-            ball.pen(speed=(ballSpeed + bounces + 1))
-        bounces = bounces + 1
+        if bounces % 2 == 0:
+            ballXDirection *= 1.2
+            ballYDirection *= 1.2
+            ball_speed += 1
 
         board.clear()
         board.write(
             "                          Score \nPlayer A: {}      Bounces: {}      Player B: {} \n                          Speed = {} ".format(
-                playerAScore, bounces, playerBScore, bounces + 1), align="center")
+                playerAScore, bounces, playerBScore, ball_speed), align="center")
+
+    window.ontimer(action, 1)
+
+
+action()
+t.mainloop()
