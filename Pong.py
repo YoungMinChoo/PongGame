48b9da0e3c22fc75a3f66becd6dada0937ae97bb
YoungMinChoo <youngminchoo001@gmail.com>
2022-03-14 22:55:23 +0900

    added pause functionality

--- Pong.py
+++ Pong.py
@@ -7,6 +7,7 @@ base_speed = 0.4
 playerAScore, playerBScore = 0, 0
 bounces = 0
 menu = True
+paused = False
 ballXDirection, ballYDirection = base_speed, base_speed
 
 # Initializations
@@ -27,17 +28,17 @@ pen.hideturtle()
 # Left Paddle
 leftPaddle = t.Turtle()
 leftPaddle.penup()
-# leftPaddle.hideturtle()
+leftPaddle.hideturtle()
 
 # Right Paddle
 rightPaddle = t.Turtle()
 rightPaddle.penup()
-# rightPaddle.hideturtle()
+rightPaddle.hideturtle()
 
 # Ball
 ball = t.Turtle()
 ball.penup()
-# ball.hideturtle()
+ball.hideturtle()
 
 # Scoreboard
 board = t.Turtle()
@@ -92,6 +93,19 @@ def button(length, width):
     pen.pensize(1)
     pen.penup()
 
+
+def toggle_pause():
+    global paused
+    if menu:
+        window.bye()
+    elif not menu and not paused:
+        paused = True
+    else:
+        paused = False
+        action()
+
+
+
 def clear_drawings():
     pen.clear()
     board.clear()
@@ -104,20 +118,20 @@ def btnclick(x,y):
     global menu
     if 37 < x < 157 and 47 > y > 12:
         print("Start Game")
-        # clear_drawings()
-        # in_game_env_setup()
+        clear_drawings()
+        in_game_env_setup()
+        menu = False
     elif 37 < x < 157 and -33 > y > -68:
         print("Rules")
         # clear_drawings()
+        # menu = False
     elif 37 < x < 157 and -113 > y > -148:
         print("Highscore")
         # clear_drawings()
+        # menu = False
     elif 37 < x < 157 and -193 > y > -223:
-        print("Quit")
-        # clear_drawings()
+        window.bye()
     print(x, y)
-    menu = False
-    return menu
 
 
 def in_game_env_setup():
@@ -157,86 +171,89 @@ def in_game_env_setup():
 
 def action():
     global playerAScore, playerBScore, bounces, ballXDirection, ballYDirection, ball_speed
-    window.update()
-    ball.setx(ball.xcor() + ballXDirection)
-    ball.sety(ball.ycor() + ballYDirection)
+    if not paused:
+        window.update()
+        ball.setx(ball.xcor() + ballXDirection)
+        ball.sety(ball.ycor() + ballYDirection)
 
-    # Border set up
-    # Vertical
-    if ball.ycor() > 290:
-        ball.sety(290)
-        ballYDirection = ballYDirection * -1
+        # Border set up
+        # Vertical
+        if ball.ycor() > 290:
+            ball.sety(290)
+            ballYDirection = ballYDirection * -1
 
-    if ball.ycor() < -290:
-        ball.sety(-290)
-        ballYDirection = ballYDirection * -1
+        if ball.ycor() < -290:
+            ball.sety(-290)
+            ballYDirection = ballYDirection * -1
 
-    # Horizontal
-    if ball.xcor() > 390:
-        ball.goto(0, 0)
-        ballXDirection, ballYDirection = base_speed, base_speed
-        ballXDirection = ballXDirection * -1
-        playerAScore = playerAScore + 1
-        bounces = 0
-        ball_speed = 1
+        # Horizontal
+        if ball.xcor() > 390:
+            ball.goto(0, 0)
+            ballXDirection, ballYDirection = base_speed, base_speed
+            ballXDirection = ballXDirection * -1
+            playerAScore = playerAScore + 1
+            bounces = 0
+            ball_speed = 1
 
-        # Score
-        board.clear()
-        board.write(
-            "                          Score \nPlayer A: {}      Bounces: {}      Player B: {} \n                          Speed = {} ".format(
-                playerAScore, bounces, playerBScore, bounces + 1), align="center")
+            # Score
+            board.clear()
+            board.write(
+                "                          Score \nPlayer A: {}      Bounces: {}      Player B: {} \n                          Speed = {} ".format(
+                    playerAScore, bounces, playerBScore, bounces + 1), align="center")
 
-    if (ball.xcor()) < -390:
-        ball.goto(0, 0)
-        ballXDirection, ballYDirection = base_speed, base_speed
-        ballXDirection = ballXDirection * -1
-        playerBScore = playerBScore + 1
-        bounces = 0
-        ball_speed = 1
+        if (ball.xcor()) < -390:
+            ball.goto(0, 0)
+            ballXDirection, ballYDirection = base_speed, base_speed
+            ballXDirection = ballXDirection * -1
+            playerBScore = playerBScore + 1
+            bounces = 0
+            ball_speed = 1
 
-        # Score
-        board.clear()
-        board.write(
-            "                          Score \nPlayer A: {}      Bounces: {}      Player B: {} \n                          Speed = {} ".format(
-                playerAScore, bounces, playerBScore, bounces + 1), align="center")
+            # Score
+            board.clear()
+            board.write(
+                "                          Score \nPlayer A: {}      Bounces: {}      Player B: {} \n                          Speed = {} ".format(
+                    playerAScore, bounces, playerBScore, bounces + 1), align="center")
 
-    # Collisions
-    if (ball.xcor() > 340) and (ball.xcor() < 350) and (
-            rightPaddle.ycor() + 40 > ball.ycor() > rightPaddle.ycor() - 40):
-        ball.setx(340)
-        ballXDirection = ballXDirection * -1
-        bounces = bounces + 1
+        # Collisions
+        if (ball.xcor() > 340) and (ball.xcor() < 350) and (
+                rightPaddle.ycor() + 40 > ball.ycor() > rightPaddle.ycor() - 40):
+            ball.setx(340)
+            ballXDirection = ballXDirection * -1
+            bounces = bounces + 1
 
-        # High bounce difficulty
-        if bounces % 2 == 0:
-            ballXDirection *= 1.2
-            ballYDirection *= 1.2
-            ball_speed += 1
+            # High bounce difficulty
+            if bounces % 2 == 0:
+                ballXDirection *= 1.2
+                ballYDirection *= 1.2
+                ball_speed += 1
 
-        # Score
-        board.clear()
-        board.write(
-            "                          Score \nPlayer A: {}      Bounces: {}      Player B: {} \n                          Speed = {} ".format(
-                playerAScore, bounces, playerBScore, ball_speed), align="center")
+            # Score
+            board.clear()
+            board.write(
+                "                          Score \nPlayer A: {}      Bounces: {}      Player B: {} \n                          Speed = {} ".format(
+                    playerAScore, bounces, playerBScore, ball_speed), align="center")
 
-    elif (ball.xcor() < -340) and (ball.xcor() > -350) and (
-            leftPaddle.ycor() + 40 > ball.ycor() > leftPaddle.ycor() - 40):
-        ball.setx(-340)
-        ballXDirection = ballXDirection * -1
-        bounces = bounces + 1
+        elif (ball.xcor() < -340) and (ball.xcor() > -350) and (
+                leftPaddle.ycor() + 40 > ball.ycor() > leftPaddle.ycor() - 40):
+            ball.setx(-340)
+            ballXDirection = ballXDirection * -1
+            bounces = bounces + 1
 
-        # High bounce difficulty
-        if bounces % 2 == 0:
-            ballXDirection *= 1.2
-            ballYDirection *= 1.2
-            ball_speed += 1
+            # High bounce difficulty
+            if bounces % 2 == 0:
+                ballXDirection *= 1.2
+                ballYDirection *= 1.2
+                ball_speed += 1
 
-        board.clear()
-        board.write(
-            "                          Score \nPlayer A: {}      Bounces: {}      Player B: {} \n                          Speed = {} ".format(
-                playerAScore, bounces, playerBScore, ball_speed), align="center")
+            board.clear()
+            board.write(
+                "                          Score \nPlayer A: {}      Bounces: {}      Player B: {} \n                          Speed = {} ".format(
+                    playerAScore, bounces, playerBScore, ball_speed), align="center")
 
-    window.ontimer(action, 1)
+        window.ontimer(action, 1)
+    else:
+        window.update()
 
 # Draw main menu and buttons
 pen.goto(37, 47)
@@ -266,7 +283,7 @@ pen.write("QUIT", font=("Arial",12,"bold"))
 window.onscreenclick(btnclick, 1)
 
 # add function for different menus depending on if in game or main menu
-window.onkeypress(window.bye, 'Escape')
+window.onkeypress(toggle_pause, 'Escape')
 
 window.listen()
 
